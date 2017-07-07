89c4fa9ed78cdf5c6b534d9b68d54bb1195d1cea
Jason Swett <jason@benfranklinlabs.com>
2017-07-07 12:00:07 -0400

    Fix ordering error.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,7 +39,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to restaurant_orders_path(@order.restaurant), notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
