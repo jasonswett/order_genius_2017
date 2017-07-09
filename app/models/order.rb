830ad5ee19861be8fae93cd456a02cd3640f56bc
Jason Swett <jason@benfranklinlabs.com>
2017-07-09 18:53:37 -0400

    Add description to order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :restaurant
   has_many :order_menu_items
+  has_many :menu_items, through: :order_menu_items
   accepts_nested_attributes_for :order_menu_items
+
+  def description
+    menu_items.map(&:name).join(', ')
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'when there are just two items' do
+    it 'lists the two items' do
+      restaurant = create(:restaurant)
+      order = create(:order)
+
+      eggs = create(:menu_item, name: 'Eggs')
+      bacon = create(:menu_item, name: 'Bacon')
+      order.menu_items << eggs
+      order.menu_items << bacon
+
+      expect(order.description).to eq('Eggs, Bacon')
+    end
+  end
 end
